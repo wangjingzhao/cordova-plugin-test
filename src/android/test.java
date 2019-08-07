863167fe358ac9292587eafa0ca1ea8b9b46a0ad
qianxun <517346462@qq.com>
2019-08-07 21:22:53 +0800

    add android test file

---
+++ src/android/test.java
@@ -0,0 +1,26 @@
+package com.wjz.test;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.PackageManager;
+
+public class WjzTest extends CordovaPlugin {
+    @Override public boolean execute (String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        try {
+            if (action.equals("test")) {
+                PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+                callbackContext.success("success");
+                return true;
+            }
+            return false;
+        } catch (NameNotFoundException e) {
+            callbackContext.success("failed");
+            return true;
+        }
+    }
+}
