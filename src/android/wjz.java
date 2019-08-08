fd7fbdb644834675add1d70f61394b486573c12a
wangjingzhao <517346462@qq.com>
2019-08-08 13:44:37 +0800

    android 测试

--- src/android/wjz.java
+++ src/android/wjz.java
@@ -2,6 +2,8 @@ package com.wjz.cordova.wjz;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
