f2d40e96318fe73908e4e888b6dce1d0291f7cba
wangjingzhao <517346462@qq.com>
2019-08-08 08:47:24 +0800

    android 测试

--- src/android/wjz.java
+++ src/android/wjz.java
@@ -5,12 +5,15 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
 
 public class wjz extends CordovaPlugin {
+    private static final String TAG = wjz.class.getSimpleName();
     @Override public boolean execute (String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d(TAG, "execute: wjz");
         try {
             if (action.equals("test")) {
                 PackageManager packageManager = this.cordova.getActivity().getPackageManager();
